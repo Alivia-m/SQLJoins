40b5b831d039f6070e3fb5ebf467abb35d2b0972
Alivia-m <85471583+Alivia-m@users.noreply.github.com>
2021-07-13 20:02:02 -0500

    Add files via upload

--- SQLjoins.sql
+++ SQLjoins.sql
@@ -41,3 +41,61 @@ SELECT e.employeeID, e.firstname, e.lastname, p.name , s.Quantity FROM sales AS 
 INNER JOIN employees AS E ON e.EmployeeID = s.EmployeeID
 INNER JOIN products AS p ON p.ProductID = s.ProductID
 ORDER BY s.Quantity desc;
+
+-- What is the total revenue from on sale products sold? 
+SELECT p.ProductID, p.name, p.price, p.onsale, SUM(s.PricePerUnit) AS Revenue FROM products AS p
+INNER JOIN sales AS s ON s.ProductID = p.ProductID;
+
+-- What is the total revenue from games?
+SELECT c.name, sum(s.PricePerUnit) AS Revenue from categories AS c
+INNER JOIN products AS p ON p.CategoryID = c.CategoryID
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+WHERE c.name = 'Games';
+
+
+-- What is the total quantity of games sold? *
+SELECT c.name, sum(s.Quantity) AS TotalQuantity from categories AS c
+INNER JOIN products AS p ON p.CategoryID = c.CategoryID
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+WHERE c.name = 'Games';
+
+-- How many products have no sales? *
+SELECT p.productid, p.name, p.price, s.Quantity AS AmountSold FROM products AS p
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+WHERE Quantity = 0;
+
+-- What is the average rating for game reviews? 
+SELECT c.name, AVG(r.Rating) AS AVGRating FROM categories AS c
+INNER JOIN products AS p ON p.CategoryID = c.CategoryID
+INNER JOIN reviews AS r ON r.ProductID = p.ProductID
+WHERE c.name = 'Games';
+
+-- Which category has the 2nd most sales (by quantity)? 
+SELECT c.CategoryID, c.name, SUM(s.Quantity) AS TotalSales FROM categories AS c 
+INNER JOIN products AS p ON p.CategoryID = c.CategoryID
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+GROUP BY c.name
+ORDER BY TotalSales DESC LIMIT 2;
+
+-- How many unique products were sold during 2015 with a price greater than 50? *
+SELECT DISTINCT count(p.name), s.date FROM products AS p
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+WHERE s.date LIKE '2015%' AND p.Price > 50;
+
+-- Which department has the lowest revenue? *
+SELECT d.name, SUM(s.PricePerUnit) AS Revenue FROM departments AS d
+INNER JOIN categories AS c ON c.DepartmentID = d.DepartmentID
+INNER JOIN products AS P ON p.CategoryID = c.CategoryID
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+GROUP BY d.name
+ORDER BY Revenue ASC LIMIT 1;
+
+-- What is the first name of the employee with the most sales of small electronics (by revenue)?
+SELECT d.name, e.FirstName, SUM(s.PricePerUnit) AS Revenue FROM departments AS d
+INNER JOIN categories AS c ON c.DepartmentID = d.DepartmentID
+INNER JOIN products AS p ON p.CategoryID = c.CategoryID
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+INNER JOIN employees AS e ON e.EmployeeID = s.EmployeeID
+WHERE d.name = 'Small Electronics'
+GROUP BY e.FirstName
+ORDER BY Revenue DESC LIMIT 1;
